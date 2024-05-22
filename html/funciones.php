8c3641824fe4d7676cb125eb3ac42896442dfd9f
nachotg14 <nachotg14@gmail.com>
2024-05-22 19:40:21 +0200

    Update funciones.php

--- html/funciones.php
+++ html/funciones.php
@@ -1,6 +1,6 @@
 <?
 
-class funciones{
+class Funciones{
     public static function validarDNI($dni_str) {
         // Eliminar espacios en blanco y convertir a mayúsculas
         $dni_str = strtoupper(str_replace(' ', '', $dni_str));
